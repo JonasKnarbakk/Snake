7b94b096ee1f5f552f7ef286136350e913923a52
Jonas Knarbakk <jonas@knarbakk.no>
2017-05-05 00:36:47 +0200

    Added handler for music

---
+++ build/src/CMakeFiles/Snake.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Snake.dir/Main.cpp.o"
+  "CMakeFiles/Snake.dir/GameManager.cpp.o"
+  "CMakeFiles/Snake.dir/InputManager.cpp.o"
+  "CMakeFiles/Snake.dir/Timer.cpp.o"
+  "CMakeFiles/Snake.dir/SDL/SDLBmp.cpp.o"
+  "CMakeFiles/Snake.dir/SDL/SDLError.cpp.o"
+  "CMakeFiles/Snake.dir/SDL/SDLManager.cpp.o"
+  "CMakeFiles/Snake.dir/Handlers/MusicHandler.cpp.o"
+  "../../bin/Snake.pdb"
+  "../../bin/Snake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Snake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
